2115d320603848629ce68f5cc932edeb9d375ba5
zreit <zisis_relas@yahoo.com>
2022-06-28 16:27:23 +0200

    db connected, todo module created

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,19 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { TodosModule } from './todos/todos.module';
+import 'dotenv/config';
+
+const DB_CONNECTION_STRING: string = process.env.DB_CONNECTION_STRING;
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(DB_CONNECTION_STRING),
+    TodosModule,
+  ],
   controllers: [],
   providers: [],
 })
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,6 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import 'dotenv/config';
+
+const PORT: number = parseInt(process.env.PORT) || 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,6 +17,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  await app.listen(PORT);
 }
 bootstrap();
