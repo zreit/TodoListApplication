242a29392c296f643f12e5865d9b6c9fd667771f
zreit <zisis_relas@yahoo.com>
2022-06-28 14:46:11 +0200

    swaggerui included, environment variable included

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('ToDoListApplication')
+    .setDescription('This is the REST API for the application')
+    .setVersion('1.0')
+    .addTag('todos')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
