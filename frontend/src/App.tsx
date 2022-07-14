f03c78a20b03d8bf01dab14075a448f854fe0735
zreit <zisis_relas@yahoo.com>
2022-07-14 19:38:18 +0200

    rough structure for frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
-import Header from './components/Header';
-import Main from './components/Main';
-import Footer from './components/Footer';
+import Header from './components/header/Header';
+import Main from './components/main/Main';
+import Footer from './components/footer/Footer';
 
 function App() {
   return (
